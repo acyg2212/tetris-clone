a0059af4e09c7f66e26b27a121695a9cfe5095ca
Aaron Cygnarowicz <64909592+acyg2212@users.noreply.github.com>
2021-01-20 11:04:21 -0500

    added a starting main funciton

--- main.py
+++ main.py
@@ -173,7 +173,7 @@ def check_lost(positions):
 
 
 def get_shape(shapes):
-    return random.choice(shapes)
+    return Piece(5, 0, random.choice(shapes))
 
 
 def draw_text_middle(text, size, color, surface):
@@ -210,16 +210,51 @@ def draw_window(surface, grid):
 
     surface.blit(label, (top_left_x + play_width/2 - label.get_width()/2, 30))
 
-    draw_grid()
+    draw_grid(surface, grid)
     pygame.display.update()
 
 
-def main():
-    pass
+def main(win):
+    locked_positions = {}
+    grid = create_grid(locked_positions)
+
+    change_piece = False
+    run = True
+    current_piece = get_shape()
+    next_piece = get_shape()
+    clock = pygame.time.Clock()
+    fall_time = 0
+
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    current_piece.x -= 1
+                    if not(valid_space(current_piece, grid)):
+                        current_piece.x += 1
+                if event.key == pygame.K_RIGHT:
+                    current_piece.x += 1
+                    if not (valid_space(current_piece, grid)):
+                        current_piece.x -= 1
+                if event.key == pygame.K_DOWN:
+                    current_piece.y += 1
+                    if not(valid_space(current_piece, grid)):
+                        current_piece.y -= 1
+                if event.key == pygame.K_UP:
+                    current_piece.rotation += 1
+                    if not (valid_space(current_piece, grid)):
+                        current_piece -= 1
+
+    draw_window(win, grid)
 
 
-def main_menu():
-    pass
+def main_menu(win):
+    main(win)
 
 
-main_menu()  # start game
+win = pygame.display.set_mode((s_width, s_height))
+pygame.display.set_caption("Tetris Clone")
+main_menu(win)  # start game
